8d320a50cf443cb0a33f0a8d79542eaf1e872049
sa001gar <kundusagar233@gmail.com>
2024-12-10 04:58:38 +0530

    Update

--- queue_management/urls.py
+++ queue_management/urls.py
@@ -4,28 +4,19 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Create API info for Swagger documentation
-api_info = openapi.Info(
-    title="Queue Management API",
-    default_version='v1',
-    description="API for Queue Management System",
-   #  terms_of_service="https://www.yourapp.com/terms/",
-   #  contact=openapi.Contact(email="contact@yourapp.com"),
-   #  license=openapi.License(name="Your License"),
-)
-
 schema_view = get_schema_view(
-    api_info,
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-    url='https://trackseries.sagarkundu.live',  # Add your domain
+   openapi.Info(
+      title="Queue Management API",
+      default_version='v1',
+      description="API for Queue Management app",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('queueapp.urls')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
