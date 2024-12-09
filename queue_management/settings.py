0e24760273ee3826bd06d326553d959715a852bd
sa001gar <kundusagar233@gmail.com>
2024-12-10 05:03:53 +0530

    Update

--- queue_management/settings.py
+++ queue_management/settings.py
@@ -32,10 +32,10 @@ ALLOWED_HOSTS = ['trackseries.sagarkundu.live','15.207.188.175', 'localhost','12
 
 CSRF_TRUSTED_ORIGINS = ['https://trackseries.sagarkundu.live',]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://trackseries.sagarkundu.live",  # Replace with the origin making the request
-#     "https://localhost:8000",  # For local development
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://trackseries.sagarkundu.live",  # Replace with the origin making the request
+    "https://localhost:8000",  # For local development
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
